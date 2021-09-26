37ec4ea0385e53b0a241b05e8af8b02c92cc54d4
Szalai <sz.szalai16@gmail.com>
2021-09-26 18:41:42 +0200

    Privát adattagok létrehozva

--- src/hu/petrik/Festmeny.java
+++ src/hu/petrik/Festmeny.java
@@ -1,4 +1,13 @@
 package hu.petrik;
 
+import java.time.LocalDateTime;
+
 public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
 }
